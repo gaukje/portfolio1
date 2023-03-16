60464a3d0f5eb5de7efec241ec622244863b858f
gautte <s362066@oslomet.no>
2023-03-16 13:35:44 +0100

    added argparse to client

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ def client(ip, port, path):
 
         # Sending content in chunks of 1000 bytes
         message = 0
-        while true:
+        while True:
             data = file.read(1000)
             if not data:
                 break
@@ -20,3 +20,20 @@ def client(ip, port, path):
 
         # Receiving acknowledgement message from server
         ack = clientSocket.recv(1000)
+
+        # Print the result
+        print("Sent {:.2f} MB to {}:{} and received acknowledgement: {}".format(
+             message / (1000 * 1000), ip, port, ack.decode()))
+
+        # Close the client socket
+        clientSocket.close()
+
+
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser(description='TCP client for sending data to the server')
+        parser.add_argument('ip', type=str, help='IP address')
+        parser.add_argument('port', type=int, help='Port number')
+        parser.add_argument('path', type=str, help='Path of the file you wish to send')
+        args = parser.parse_args()
+
+        client(args.ip, args.port, args.path)
