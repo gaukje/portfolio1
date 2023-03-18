1fc72f2a1052d8f57230f298b5173e81543e9f2a
gautte <s362066@oslomet.no>
2023-03-18 10:57:56 +0100

    Changed the server page

--- simpleperf.py
+++ simpleperf.py
@@ -1,56 +1,63 @@
-import socket
-import threading
-import time
 import argparse
+import socket
+import time
 
 
-def handle_client(client_socket, client_address):
-    # Receive the data in chunks of 1000 bytes and calculate the amount of data received
-    start_time = time.time()
-    received_data = 0
-    while True:
-        data = client_socket.recv(1000)
-        if not data:
-            break
-        received_data += len(data)
+def server(host, port):
+    # Create a socket
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Calculate the bandwidth and print the result
-    elapsed_time = time.time() - start_time
-    bandwidth = received_data / elapsed_time / (1024 * 1024)
-    print("Received {:.2f} MB from {} in {:.2f} seconds ({:.2f} Mbps)".format(
-        received_data / (1024 * 1024), client_address, elapsed_time, bandwidth))
-
-    # Send the acknowledgement message to the client
-    client_socket.send("ACK:BYE".encode())
-
-    # Close the client socket
-    client_socket.close()
-
-
-def server_mode(ip_address, port_number, file_size):
-    # Create a TCP socket and bind it to the IP address and port number
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((ip_address, port_number))
+    # Bind the socket to the specified host and port
+    serverSocket.bind((host, port))
 
     # Listen for incoming connections
-    server_socket.listen(5)
-    print("Server is listening on {}:{}".format(ip_address, port_number))
+    serverSocket.listen(1)
 
-    while True:
-        # Accept the incoming connection and create a new thread to handle it
-        client_socket, client_address = server_socket.accept()
-        print("Client {} is connected".format(client_address))
-        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-        client_thread.start()
+    print(f"---------------------------------------------")
+    print(f"A simpleperf server is listening on port {port}")
+    print(f"---------------------------------------------")
 
-    # Close the server socket
-    server_socket.close()
+    # Accept a connection
+    connection, client_address = serverSocket.accept()
+
+    total_received_bytes = 0
+    start_time = time.time()
+
+    try:
+        while True:
+            # Receive data in chunks of 1000 bytes
+            data = connection.recv(1000)
+            total_received_bytes += len(data)
+
+            if not data:
+                break
+
+    finally:
+        end_time = time.time()
+        connection.close()
+
+        elapsed_time = end_time - start_time
+        received_kilobytes = total_received_bytes / 1000
+        received_megabytes = received_kilobytes / 1000
+
+        bandwidth_kbps = received_kilobytes / elapsed_time
+        bandwidth_mbps = received_megabytes / elapsed_time
+
+        print(f"Received {received_megabytes:.2f} MB in {elapsed_time:.2f} seconds")
+        print(f"Bandwidth: {bandwidth_kbps:.2f} Kbps ({bandwidth_mbps:.2f} Mbps)")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='TCP server for receiving data from clients')
-    parser.add_argument('ip_address', type=str, help='IP address to bind the server socket to')
-    parser.add_argument('port_number', type=int, help='Port number to bind the server socket to')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A simpleperf server")
+
+    parser.add_argument("-s", "--server", action="store_true", help="enable the server mode")
+    parser.add_argument("-b", "--bind", type=str, default="0.0.0.0", help="IP address of the server's interface")
+    parser.add_argument("-p", "--port", type=int, default=8080, help="port number on which the server should listen")
+    parser.add_argument("-f", "--format", type=str, choices=["B", "KB", "MB"], default="MB", help="format of summary of results")
+
     args = parser.parse_args()
 
-    server_mode(args.ip_address, args.port_number, 0)
+    if args.server:
+        server(args.bind, args.port)
+    else:
+        print("Please specify server mode with -s or --server")
