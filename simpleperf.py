6eecd3e92909d74b9c62832e722e60ea2cb0cebe
gautte <s362066@oslomet.no>
2023-03-18 13:51:40 +0100

    Updated simpleperf.py to handle -i -n -p

--- simpleperf.py
+++ simpleperf.py
@@ -19,7 +19,6 @@ def server(serverIp, serverPort, formatUnit):
             receivedBytes = 0
             startTime = time.time()
 
-
             while True:
                 data = connection.recv(1000)
                 if data == b"BYE":
@@ -27,7 +26,6 @@ def server(serverIp, serverPort, formatUnit):
                     break
 
                 receivedBytes += len(data)
-
 
             endTime = time.time()
             connection.close()
@@ -55,7 +53,8 @@ def server(serverIp, serverPort, formatUnit):
     except ConnectionError as e:
         print(f"Failed to connect to server: {e}")
 
-def client(serverIp, serverPort, duration):
+
+def client(serverIp, serverPort, duration, interval, parallel, numBytes=None):
     try:
         with socket(AF_INET, SOCK_STREAM) as clientSocket:
             clientSocket.connect((serverIp, serverPort))
@@ -67,11 +66,16 @@ def client(serverIp, serverPort, duration):
 
             sentBytes = 0
             startTime = time.time()
+            lastIntervalTime = startTime
 
-            while time.time() - startTime < duration:
+            while (time.time() - startTime < duration) and ():
                 data = b'0' * 1000
                 clientSocket.sendall(data)
                 sentBytes += len(data)
+
+            if interval and time.time() - lastIntervalTime >= interval:
+                printInterval(clientSocket, startTime, sentBytes, serverIp, serverPort)
+                lastIntervalTime = time.time()
 
             clientSocket.sendall(b"BYE")
             ack = clientSocket.recv(1024)
@@ -89,6 +93,39 @@ def client(serverIp, serverPort, duration):
         print(f"Connection lost during transfer: {e}")
         sys.exit(1)
 
+
+def parseNumBytes(num_str):
+    units = {'B': 1, 'KB': 1000, 'MB': 1000 * 1000}
+    last_char = num_str[-1]
+    if last_char in units:
+        num = int(num_str[:-1]) * units[last_char]
+    else:
+        num = int(num_str)
+    return num
+
+
+# ... (other parts of the code)
+
+def printInterval(clientSocket, startTime, sentBytes, serverIp, serverPort):
+    timeElapsed = time.time() - startTime
+    sentMB = sentBytes / 1000 / 1000
+    bandwidthMPBS = sentMB / timeElapsed
+
+    headers = ["ID", "Interval", "Transfer", "Bandwidth"]
+    dataRow = [
+        f"{serverIp}:{serverPort}",
+        f"{timeElapsed:.2f} - {timeElapsed + interval:.2f}",
+        f"{sentMB:.2f} MB",
+        f"{bandwidthMPBS:.2f} Mbps"
+    ]
+
+    tableData = [headers, dataRow]
+    max_widths = [max(map(len, col)) for col in zip(*tableData)]
+
+    for row in tableData:
+        print(" ".join((val.ljust(width) for val, width in zip(row, max_widths))))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A simpleperf tool")
 
@@ -96,20 +133,25 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--client", action="store_true", help="enable the client mode")
     parser.add_argument("-b", "--bind", type=str, default="0.0.0.0", help="IP address of the server's interface")
     parser.add_argument("-p", "--port", type=int, default=8080, help="port number on which the server should listen")
-    parser.add_argument("-f", "--format", type=str, choices=["B", "KB", "MB"], default="MB", help="format of summary of results")
+    parser.add_argument("-f", "--format", type=str, choices=["B", "KB", "MB"], default="MB",
+                        help="format of summary of results")
     parser.add_argument("-I", "--server_ip", type=str, help="IP address of the server")
     parser.add_argument("-t", "--time", type=int, help="Total duration for which data should be generated")
+    parser.add_argument("-i", "--interval", type=int, help="print statistics per z second")
+    parser.add_argument("n", "--num", type=str, help="Number of bytes")
+    parser.add_argument("-P", "--parallel", type=int, choices=range(1, 6), default=1,
+                        help="creates parallel connections to connect to the server and send data - it must be 1 and the max value should be 5 - default:1")
 
     args = parser.parse_args()
 
-    if args.server and args.client:
-        print("Please choose either server mode with -s or --server, or client mode with -c or --client")
-    elif args.server:
+    if args.server:
         server(args.bind, args.port, args.format)
     elif args.client:
-        if args.server_ip is None or args.time is None:
-            print("Please provide server IP with -I or --server_ip, and duration with -t or --time")
+        if args.num:
+            numBytes = parseNumBytes(args.num)
         else:
-            client(args.server_ip, args.port, args.time)
+            numBytes = None
+
+        client(args.server_ip, args.port, args.time, args.interval, args.parallel, numBytes)
     else:
-        print("Please specify server mode with -s or --server, or client mode with -c or --client")
+        print("Please specify server mode with -s or --server")
